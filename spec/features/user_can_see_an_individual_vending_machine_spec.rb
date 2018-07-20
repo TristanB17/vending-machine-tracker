3445189716fe0b996d4353c083cda0fbd94e3b4d
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-20 10:44:47 -0600

    feature test for dons made

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -12,9 +12,16 @@ feature 'When a user visits a vending machine show page' do
   it 'sees a list of individual machine attributes' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: 'Cheetos', price: '400')
+    snack_2 = dons.snacks.create(name: 'Fritos', price: '200')
+    expected_avg = 300
 
     visit machine_path(dons)
 
-    
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+    expect(page).to have_content(snack_2.name)
+    expect(page).to have_content(snack_2.price)
+    expect(page).to have_content(expected_avg)
   end
 end
